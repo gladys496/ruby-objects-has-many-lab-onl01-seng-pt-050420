00fa28c80726a17c129c62f5a5db7db0a4259285
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 06:23:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,13 @@ class Artist
   end
 
 
-
-
-  def add_song(song)
+def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
+
+
 
   def add_song_by_name(name)
     song = Song.new(name)
