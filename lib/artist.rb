506edd6251f6baab0785173aa02e94fc62a6b973
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 06:37:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,12 @@ class Artist
 def add_song(song)
     @songs << song
     song.artist = self
-   
+    @@song_count += 1
   end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end 
 
 
   def add_song_by_name(name)
