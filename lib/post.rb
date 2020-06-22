792907f0639d24a4922ffbbdc3cc4eb077d49a88
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 06:05:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,15 @@
-class Post 
-  
-  attr_accessor :title 
-  
-  @@all = []
-  def initalize(title)
-    @title = title 
-    @@all << self 
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
   end
-  def self.all
-    @@all 
-  end 
-end 
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
